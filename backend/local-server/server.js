53a42cf9e37aabf9a454566bf974bee803fe5c92
Amogh <awkwardcandidate@gmail.com>
2020-07-31 13:06:26 -0700

    added static files within local-server

--- backend/local-server/server.js
+++ backend/local-server/server.js
@@ -13,7 +13,7 @@ app.use((req,res, next)=>{
 
 app.use(cors())
 
-app.use(express.static('../static'))
+app.use(express.static('./static'))
 
 app.listen(PORT,()=>{
   console.log(`server started on port: ${PORT}`)
